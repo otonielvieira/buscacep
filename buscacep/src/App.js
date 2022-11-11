dac346c0063ecc1342e5ab7fb8795094f04a7b7d
otonielvieira <ottobuzios@gmail.com>
2022-11-10 21:29:17 -0300

    Criando a tela inicial

--- buscacep/src/App.js
+++ buscacep/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>BUSCA CEP</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          QuaL<code> CEP</code> você deseja consultar ?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <input /><br />
+          <button>BUSCAR</button>
       </header>
     </div>
   );
